60af5e60830296a69f6980bdc635ff97aa7e49cc
ADT109119 <ADT109119@users.noreply.github.com>
2026-07-18 03:10:57 +0000

    deploy: b14758db812219a364897cac1fabf792eb165a63

---
+++ assets/index-legacy-OynKi7KQ.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy-S3mqUEtc.js","./index-legacy-CPahGcSA.js"],function(e,t){"use strict";var i,n;return{setters:[e=>{i=e._},e=>{n=e.registerPlugin}],execute:function(){e("GoogleAuth",n("GoogleAuth",{web:()=>i(()=>t.import("./web-legacy-BjPOgtBN.js"),void 0).then(e=>new e.GoogleAuthWeb)}))}}});
