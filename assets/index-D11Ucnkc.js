fc33ba63f74b7cd43614232a929e395f2786874b
ADT109119 <ADT109119@users.noreply.github.com>
2026-08-03 08:26:32 +0000

    deploy: 57263bb8eac7da7547a09a3b56b5d818c821c58b

---
+++ assets/index-D11Ucnkc.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C3gcsEfj.js","assets/index-DxKlpcEN.js"])))=>i.map(i=>d[i]);
+import{_ as p}from"./main-CfnDz-dh.js";import{registerPlugin as r}from"./index-DxKlpcEN.js";const _=r("App",{web:()=>p(()=>import("./web-C3gcsEfj.js"),__vite__mapDeps([0,1])).then(e=>new e.AppWeb)});export{_ as App};
